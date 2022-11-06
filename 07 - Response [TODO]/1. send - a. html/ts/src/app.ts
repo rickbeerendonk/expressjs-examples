f9526949bd1eeefc5d54c58e3a3dcf7503e4fd14
Rick Beerendonk <rick@beerendonk.com>
2022-11-06 15:39:17 +0100

    Add "response - send - html" example.

---
+++ 07 - Response [TODO]/1. send - a. html/ts/src/app.ts
@@ -0,0 +1,21 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// Responde send
+
+app.use((req, res, next) => {
+  // Response type will automatically be text/html
+  res.send("Express");
+});
+
+// Start
+
+app.listen(port, () => {
+  const url = `http://localhost:${port}`;
+  console.log(`Express app listening at ${url}`);
+});
