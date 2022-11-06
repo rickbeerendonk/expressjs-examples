cb9caf4783a2f66c18dc150e4d2df56dc251ac53
Rick Beerendonk <rick@beerendonk.com>
2022-11-06 14:00:06 +0100

    Add "Writing Middleware - Multiple" example.

---
+++ 04 - Writing Middleware/2. Multiple/ts/src/app.ts
@@ -0,0 +1,47 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// Middleware
+
+app.use(function (req, res, next) {
+  console.log(`LOGGER 1 (before next()): ${req.method} ${req.url}`);
+  next();
+  console.log(`LOGGER 1 (after next()): ${req.method} ${req.url}`);
+});
+
+app.use(function (req, res, next) {
+  console.log(`LOGGER 2 (before next()): ${req.method} ${req.url}`);
+  next();
+  console.log(`LOGGER 2 (after next()): ${req.method} ${req.url}`);
+});
+
+// Paths
+
+app.get("/", (req, res) => {
+  console.log(`GET handler: ${req.method} ${req.url}`);
+  res.send("GET at /");
+});
+
+app.get("/user", (req, res) => {
+  console.log(`GET handler: ${req.method} ${req.url}`);
+  res.send("GET at /user");
+});
+
+// Methods
+
+app.post("/", (req, res) => {
+  console.log(`POST handler: ${req.method} ${req.url}`);
+  res.send("POST at /");
+});
+
+// Start
+
+app.listen(port, () => {
+  const url = `http://localhost:${port}`;
+  console.log(`Express app listening at ${url}`);
+});
