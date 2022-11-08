68ac05301c91e33a92ae42a19421807a0210948d
Rick Beerendonk <rick@beerendonk.com>
2022-11-08 23:07:25 +0100

    Add "Setup - Watch" example.

---
+++ 99 - Setup/a. Watch/ts/src/app.ts
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Express!");
+});
+
+app.listen(port, () => {
+  const url = `http://localhost:${port}`;
+  console.log(`Express app listening at ${url}`);
+});
